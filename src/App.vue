d81cf4d7b85e8e6f7f9f4820f3a99b5dc4ed11c3
Jeongeun Lee <jungeun9005@gmail.com>
2023-06-19 15:28:57 -0700

    edit vue

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,28 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap'); 
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Fira Sans", sans-serif;
+}
+.header-static {
+  background-image: url(../src/assets/header/tools.jpg);
+  height: 40vh;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.header-section {
+  background-color: rgba(255,255,255,.3);
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding-left: 10%;
+  color: #439A97;
+  font-weight: 800;
+  letter-spacing: 3px;
+}
 </style>
