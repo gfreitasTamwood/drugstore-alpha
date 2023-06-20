ca32ff68dd615cdc12968d829ab14036ce1f4ab1
Jeongeun Lee <jungeun9005@gmail.com>
2023-06-20 15:23:56 -0700

    seperate css file

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <NavComponent/>
+  <HelloWorld/>
   <router-view/>
 </template>
 
 <script>
-import NavComponent from './components/NavComponent.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    NavComponent
+    HelloWorld
   }
 }
 </script>
@@ -22,34 +22,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.navbar {
-  background-color: #97DECE;
-  height: 8vh;
-}
-.navbar > ul {
-  height: 100%;
-  display: flex;
-  justify-content: space-evenly;
-  padding-inline-start: 0px;
-}
-.navbar li {
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-
-}
-.navbar a {
-    height: 100%;
-    width: 100%;
-    color: #439A97;
-    text-decoration: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-.navbar a:hover {
-  background-color: #CBEDD5;
 }
 </style>
