1769e8699c33897c4d2ff8c123f98df604fb7715
Gabriel <bielpch@gmail.com>
2023-06-20 17:46:46 -0700

    First Commit Footer

---
+++ footer/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <FooterComponent/>
+</template>
+
+<script>
+import FooterComponent from './components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+  FooterComponent
+  }
+}
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap'); 
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Fira Sans", sans-serif;
+}
+
+.footer {
+    background-color: #f5f5f5;
+    padding: 20px;
+    text-align: center;
+  }
+  
+  .footer-links {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px;
+  }
+  
+  .footer-column {
+    flex: 1;
+    margin-right: 20px;
+  }
+  
+  .footer-column h4 {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+  
+  .footer-column ul {
+    list-style: none;
+    padding: 0;
+  }
+  
+  .footer-column ul li {
+    margin-bottom: 5px;
+  }
+  
+  .footer-column ul li a {
+    color: #333;
+    text-decoration: none;
+  }
+  
+  .copy-info {
+    font-size: 14px;
+    color: #777;
+  }
+
+</style>
---
+++ footer/src/components/Footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer-links">
+        <div class="footer-column">
+          <h4>Customer Service</h4>
+          <ul>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Returns & Exchanges</a></li>
+            
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>Offers</h4>
+          <ul>
+            <li><a href="#">Weekly Deals</a></li>
+            <li><a href="#">Promotions</a></li>
+            <li><a href="#">Discounts</a></li>
+          </ul>
+        </div>
+        <div class="footer-column">
+          <h4>About</h4>
+          <p> Corporate Gift Cards</p>
+          <p> Carrers  </p>
+          <p> Pharmacy Acquisition</p>
+          <p> Legal </p>
+          <p> Privacy </p>
+        </div>
+        <div class="footer-column">
+          <h4> More By Alpha Drugstore</h4>
+          <p> Community Care </p>
+          <p>  Gift Cards </p>
+        </div>
+      </div>
+      <p class="copy-info">&copy; 2023 Alpha Drugstore. All rights reserved.</p>
+    </div>
+  </footer>
+
+  </template>
+        
+
+
+<script>
+export default {
+    name: 'FooterComponent'
+}
+</script>
