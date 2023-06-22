3bbe21fed168ae000a7933a1af70ec731900b462
Gabriel <bielpch@gmail.com>
2023-06-21 20:52:09 -0700

    Starting catalog

---
+++ catalog/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+ <Catalog-filters/>
+</template>
+
+<script>
+import CatalogFilters from './components/Catalog-filters.vue';
+
+export default {
+  name: 'App',
+  components: {
+   CatalogFilters
+  }
+}
+</script>
+
+
+
+
+<style>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap'); 
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Fira Sans", sans-serif;
+}
+
+.catalog-filters {
+  background-color: #f5f5f5;
+  padding: 10px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.catalog-filters h2 {
+    display: block;
+    font-size: 1.5em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+}
+
+
+.catalog-filters h3 {
+  font-size: 1rem;
+  margin-top: 15px;
+}
+
+.catalog-filters ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.catalog-filters li {
+  margin-bottom: 5px;
+}
+
+.catalog-filters li a {
+  color: #333;
+  text-decoration: none;
+}
+
+.catalog-filters li a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ catalog/src/components/Catalog-filters.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="catalog-filters">
+  <aside>
+<li>
+
+    <span class="menu-icon"> Shop By Category </span>
+</li>
+    <h2>Filters</h2>
+
+    <!-- Skincare Filters -->
+    <h3>Skincare</h3>
+    <ul>
+      <li><router-link to="/skincare/cleanser">Cleanser</router-link></li>
+      <li><router-link to="/skincare/moisturizer">Moisturizer</router-link></li>
+      <li><router-link to="/skincare/serum">Serum</router-link></li>
+      <!-- Add more skincare filter options as needed -->
+    </ul>
+
+    <!-- Makeup Filters -->
+    <h3>Makeup</h3>
+    <ul>
+      <li><router-link to="/makeup/foundation">Foundation</router-link></li>
+      <li><router-link to="/makeup/lipstick">Lipstick</router-link></li>
+      <li><router-link to="/makeup/eyeshadow">Eyeshadow</router-link></li>
+      <!-- Add more makeup filter options as needed -->
+    </ul>
+
+    <!-- Bath & Body Filters -->
+    <h3>Bath & Body</h3>
+    <ul>
+      <li><router-link to="/bath-body/bodywash">Body Wash</router-link></li>
+      <li><router-link to="/bath-body/lotion">Lotion</router-link></li>
+      <li><router-link to="/bath-body/scrub">Scrub</router-link></li>
+      <!-- Add more bath & body filter options as needed -->
+    </ul>
+
+    <!-- Other Filters -->
+    <!-- Add more filter categories as needed -->
+
+    </aside>
+    </div>
+  </template>
+
+<script>
+
+
+
+export default{
+
+    name:'Catalog-filters'
+}
+</script>
