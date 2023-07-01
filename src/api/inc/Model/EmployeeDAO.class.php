72d1e2df189fcdc621e9045a4559621f33ebeaf1
Gustavo Tamwood <gfreitas@tamwoodonline.com>
2023-06-30 18:16:38 -0700

    Code refactored. StdConverter added

--- src/api/inc/Model/EmployeeDAO.class.php
+++ src/api/inc/Model/EmployeeDAO.class.php
@@ -1,13 +1,21 @@
 <?php
 
 class EmployeeDAO {
+
     private static $db;
 
+    /**
+     * @return void
+     */
     public static function startDb() {
         self::$db = new PDOAgent("Employee");
     }
 
-    public static function getAllEmployees() {
+    /**
+     * @return Employee[]
+     */
+    public static function getAllEmployees() : array {
+        
         $sql = "SELECT * FROM employee";
         self::$db->query($sql);
 
@@ -16,7 +24,11 @@ class EmployeeDAO {
         return self::$db->resultSet();
     }
 
-    public static function deleteEmployeeById(int $employeeId) {
+    /**
+     * @param integer $employeeId
+     * @return int $rowCount
+     */
+    public static function deleteEmployeeById(int $employeeId) : int {
         $sql = "DELETE FROM employee WHERE id=:employeeId";
 
         self::$db->query($sql);
@@ -24,29 +36,43 @@ class EmployeeDAO {
         self::$db->execute();
         return self::$db->rowCount();
     }
-    public static function updateEmployee(Employee $employee) {
-        $sql = "UPDATE employee SET firstName=:firstName, lastName=:lastName, email=:email, phone=:phone, username=:username, password=:passowrd WHERE id=:id";
+
+    /**
+     * @param Employee $employee
+     * @return int $id
+     */
+    public static function updateEmployee(Employee $employee) : int {
+        $sql = "UPDATE employee SET firstName=:firstName, lastName=:lastName, field=:field email=:email, phone=:phone, avatar=:avatar, username=:username, password=:passowrd WHERE id=:id";
 
         self::$db->query($sql);
         self::$db->bind(":id",$employee->getId());
         self::$db->bind(":firstName",$employee->getFirstName());
         self::$db->bind(":lastName",$employee->getLastName());
+        self::$db->bind(":field",$employee->getField());
         self::$db->bind(":email",$employee->getEmail());
         self::$db->bind(":phone",$employee->getPhone());
+        self::$db->bind(":avatar",$employee->getAvatar());
         self::$db->bind(":username",$employee->getUsername());
         self::$db->bind(":password",$employee->getPassword());
         self::$db->execute();
 
         return self::$db->lastInsertId();
     }
-    public static function insertEmployee( Employee $employee ){
-        $sql = "INSERT INTO employee(firstName,lastName,email,phone,username,password) VALUES (:firstName,:lastName,:email,:phone,:username,:password)";
+
+    /**
+     * @param Employee $employee
+     * @return int $id
+     */
+    public static function insertEmployee( Employee $employee ) : int {
+        $sql = "INSERT INTO employee(firstName,lastName,field,email,phone,avatar,username,password) VALUES (:firstName,:lastName,:field,:email,:phone,:avatar,:username,:password)";
 
         self::$db->query($sql);
         self::$db->bind(":firstName", $employee->getFirstName());
         self::$db->bind(":lastName", $employee->getLastName());
+        self::$db->bind(":field", $employee->getField());
         self::$db->bind(":email",$employee->getEmail());
         self::$db->bind(":phone",$employee->getPhone());
+        self::$db->bind(":avatar",$employee->getAvatar());
         self::$db->bind(":username",$employee->getUsername());
         self::$db->bind(":password",$employee->getPassword());
         self::$db->execute();
@@ -54,7 +80,11 @@ class EmployeeDAO {
         return self::$db->lastInsertId();
     } 
 
-    public static function getSingleEmployee(int $id) {
+    /**
+     * @param integer $id
+     * @return Employee
+     */
+    public static function getSingleEmployee(int $id) : Employee {
         $sql = "SELECT * FROM employee WHERE id = :id";
         self::$db->query($sql);
         self::$db->bind(":id",$id);
